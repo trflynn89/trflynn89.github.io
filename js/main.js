33d79ce6134ca83f316abc769c1d4df00bcfc3a7
trflynn89 <timothy.flynn@outlook.com>
2014-07-27 22:29:11 -0400

    Static travel images
    Original site loaded these as-needed with php. Can't use php here, so
    load them all on document.ready for now

--- js/main.js
+++ js/main.js
@@ -88,16 +88,6 @@ function loadPopup(nav)
 			advanced:
 			{
 				updateOnContentResize: true
-			},
-			callbacks:
-			{
-				whileScrolling: function()
-				{
-					if (nav == Nav.Travel)
-					{
-						updateTravelPopup();
-					}
-				}
 			}
 		});
 	}
@@ -248,63 +238,53 @@ $(document).ready(function()
 
 var TravelStatus = { None : 0, Preview : 1, Enlarged : 2, Resizing : 3 };
 
-var servicingRequest = false;
 var currStatus = TravelStatus.None;
 var currText = "";
-var currPage = 1;
 var currId = "";
-var imageLimit;
-var imageCount;
-var maxPages;
 
-function setTravelParams(lim, cnt)
+function loadTravelImages()
 {
-	imageLimit = lim;
-	imageCount = cnt;
-	maxPages = Math.ceil(cnt / imageLimit);
-}
+	var res = '';
+	
+	res += '<div><img src="img/travel/001.jpg" class="image" id="trav-1" data-loc="Great Barrier Reef, Australia" alt = "" /></div>'
+	res += '<div><img src="img/travel/002.jpg" class="image" id="trav-2" data-loc="Paradise, New Zealand" alt = "" /></div>'
+	res += '<div><img src="img/travel/003.jpg" class="image" id="trav-3" data-loc="Oahu, New Zealand" alt = "" /></div>'
+	res += '<div><img src="img/travel/004.jpg" class="image" id="trav-4" data-loc="Queenstown, New Zealand" alt = "" /></div>'
+	res += '<div><img src="img/travel/005.jpg" class="image" id="trav-5" data-loc="St. Kilda Beach - Melbourne, Victoria, Australia" alt = "" /></div>'
+	res += '<div><img src="img/travel/006.jpg" class="image" id="trav-6" data-loc="Melbourne Zoo - Melbourne, Victoria, Australia" alt = "" /></div>'
+	res += '<div><img src="img/travel/007.jpg" class="image" id="trav-7" data-loc="Melbourne Zoo - Melbourne, Victoria, Australia" alt = "" /></div>'
+	res += '<div><img src="img/travel/008.jpg" class="image" id="trav-8" data-loc="The Twelve Apostles - Victoria, Australia" alt = "" /></div>'
+	res += '<div><img src="img/travel/009.jpg" class="image" id="trav-9" data-loc="Grampians National Park - Victoria, Australia" alt = "" /></div>'
+	res += '<div><img src="img/travel/010.jpg" class="image" id="trav-10" data-loc="Grampians National Park - Victoria, Australia" alt = "" /></div>'
+	res += '<div><img src="img/travel/011.jpg" class="image" id="trav-11" data-loc="Melbourne Cricket Ground - Melbourne, Victoria, Australia" alt = "" /></div>'
+	res += '<div><img src="img/travel/012.jpg" class="image" id="trav-12" data-loc="Werribee Open Range Zoo - Werribee, Victoria, Australia" alt = "" /></div>'
+	res += '<div><img src="img/travel/013.jpg" class="image" id="trav-13" data-loc="Healesville Sanctuary - Healesville, Victoria, Australia" alt = "" /></div>'
+	res += '<div><img src="img/travel/014.jpg" class="image" id="trav-14" data-loc="Healesville Sanctuary - Healesville, Victoria, Australia" alt = "" /></div>'
+	res += '<div><img src="img/travel/015.jpg" class="image" id="trav-15" data-loc="Blue Mountains - New South Wales, Australia" alt = "" /></div>'
+	res += '<div><img src="img/travel/016.jpg" class="image" id="trav-16" data-loc="Sydney Opera House - Sydney, New South Wales, Australia" alt = "" /></div>'
+	res += '<div><img src="img/travel/017.jpg" class="image" id="trav-17" data-loc="Sydney Opera House - Sydney, New South Wales, Australia" alt = "" /></div>'
+	res += '<div><img src="img/travel/018.jpg" class="image" id="trav-18" data-loc="Harbour Bridge - Sydney, New South Wales, Australia" alt = "" /></div>'
+	res += '<div><img src="img/travel/019.jpg" class="image" id="trav-19" data-loc="Flynn Reef - Great Barrier Reef, Australia" alt = "" /></div>'
+	res += '<div><img src="img/travel/020.jpg" class="image" id="trav-20" data-loc="Flynn Reef - Great Barrier Reef, Australia" alt = "" /></div>'
+	res += '<div><img src="img/travel/021.jpg" class="image" id="trav-21" data-loc="Cook Strait - New Zealand" alt = "" /></div>'
+	res += '<div><img src="img/travel/022.jpg" class="image" id="trav-22" data-loc="New Zealand" alt = "" /></div>'
+	res += '<div><img src="img/travel/023.jpg" class="image" id="trav-23" data-loc="New Zealand" alt = "" /></div>'
+	res += '<div><img src="img/travel/024.jpg" class="image" id="trav-24" data-loc="Takaro Road, New Zealand" alt = "" /></div>'
+	res += '<div><img src="img/travel/025.jpg" class="image" id="trav-25" data-loc="New Zealand" alt = "" /></div>'
+	res += '<div><img src="img/travel/026.jpg" class="image" id="trav-26" data-loc="Milford Sound, New Zealand" alt = "" /></div>'
+	res += '<div><img src="img/travel/027.jpg" class="image" id="trav-27" data-loc="Milford Sound, New Zealand" alt = "" /></div>'
+	res += '<div><img src="img/travel/028.jpg" class="image" id="trav-28" data-loc="New Zealand" alt = "" /></div>'
+	res += '<div><img src="img/travel/029.jpg" class="image" id="trav-29" data-loc="New Zealand" alt = "" /></div>'
+	res += '<div><img src="img/travel/030.jpg" class="image" id="trav-30" data-loc="New Zealand" alt = "" /></div>'
+	res += '<div><img src="img/travel/031.jpg" class="image" id="trav-31" data-loc="New Zealand" alt = "" /></div>'
+	res += '<div><img src="img/travel/032.jpg" class="image" id="trav-32" data-loc="New Zealand" alt = "" /></div>'
+	res += '<div><img src="img/travel/033.jpg" class="image" id="trav-33" data-loc="New Zealand" alt = "" /></div>'
+	res += '<div><img src="img/travel/034.jpg" class="image" id="trav-34" data-loc="Pawtuckaway State Park - New Hampshire, US" alt = "" /></div>'
+	res += '<div><img src="img/travel/035.jpg" class="image" id="trav-35" data-loc="Pawtuckaway State Park - New Hampshire, US" alt = "" /></div>'
+	res += '<div><img src="img/travel/036.jpg" class="image" id="trav-36" data-loc="Townsville, Queensland, Australia" alt = "" /></div>'
+	res += '<div><img src="img/travel/037.jpg" class="image" id="trav-37" data-loc="Cozumel, Mexico" alt = "" /></div>'
 
-function sendRequest()
-{
-	for (var i = ((currPage - 1) * imageLimit) + 1; (i <= (currPage * imageLimit)) && (i <= imageCount); ++i)
-	{
-		file = 'img/travel/';
-		id = 'trav-' + i.toString();
-
-		if (i < 10)
-		{
-			file += '00' + i.toString() + '.jpg';
-		}
-		else
-		{
-			file += '0' + i.toString() + '.jpg';
-		}
-
-		var res = '<div>';
-		res += '<img src="' + file + '" class="image" id="' + id + '" data-loc="loc" alt="" />';
-		res += '</div>';
-
-		$('#travelDiv').append(res);
-	}
-
-	servicingRequest = false;
-}
-
-function updateTravelPopup()
-{
-	if (servicingRequest)
-	{
-		return;
-	}
-
-	if ((mcs.topPct > 80) && (currStatus == TravelStatus.Preview))
-	{
-		if (++currPage <= maxPages) 
-		{
-			servicingRequest = true;
-			sendRequest();
-		}
-	}
+	$('#travelDiv').append(res);
 }
 
 $(document).ready(function()
@@ -312,7 +292,7 @@ $(document).ready(function()
 	if (currStatus == TravelStatus.None)
 	{
 		currStatus = TravelStatus.Preview;
-		sendRequest();
+		loadTravelImages();
 	}
 });
 
