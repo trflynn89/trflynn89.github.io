23fb718dee7652a6bb22ef9cbb77033bb57d9615
Tim Flynn <trflynn89@gmail.com>
2016-03-07 21:48:28 -0500

    Load travel images on document ready

--- js/travel.js
+++ js/travel.js
@@ -10,6 +10,8 @@
 
 	$(function() {
 
+		loadTravelImages();
+
 		var	$window = $(window),
 			$body = $('body'),
 			$wrapper = $('#wrapper');
@@ -287,7 +289,107 @@
 						.on('+xsmall', function() {
 							$main[0]._poptrox.windowMargin = 0;
 						});
-
 	});
-
 })(jQuery);
+
+/**
+ * Pad an integer value with 0's until its string length is >= length.
+ */
+function pad0(val, length)
+{
+    val = val.toString();
+
+    while (val.length < length)
+    {
+        val = '0' + val;
+    }
+
+    return val;
+}
+
+/**
+ * Construct the HTML div to hold images.
+ */
+function getImageDiv(imageId, extension, header, location)
+{
+    var html = ''
+
+    var imageName = pad0(imageId, 4) + '.' + extension;
+	var thumbPath = '/img/travel/thumb/' + imageName;
+	var fullPath = '/img/travel/full/' + imageName;
+
+    html += '<article class="thumb">';
+    html += '<a href="' + fullPath + '" class="image">';
+    html += '<img src="' + thumbPath + '" alt="" />';
+    html += '</a>'
+    html += '<h2>' + header + '</h2>'
+
+    if (location !== undefined)
+    {
+	    html += '<p>' + location + '</p>'
+    }
+
+    html += '</article>'
+
+    return html;
+}
+
+/**
+ * Construct the HTMLfor all travel images.
+ */
+function loadTravelImages()
+{
+	var html = '';
+
+    html += getImageDiv(41, 'png', 'Jackson Point', 'Little Cayman, Cayman Islands');
+    html += getImageDiv(42, 'png', 'Jackson Point', 'Little Cayman, Cayman Islands');
+    html += getImageDiv(43, 'jpg', 'Bus Stop', 'Little Cayman, Cayman Islands');
+    html += getImageDiv(44, 'jpg', 'Bus Stop', 'Little Cayman, Cayman Islands');
+    html += getImageDiv(45, 'jpg', 'Central Caribbean Marine Institute', 'Little Cayman, Cayman Islands');
+    html += getImageDiv(46, 'jpg', 'Point of Sand', 'Little Cayman, Cayman Islands');
+    html += getImageDiv(49, 'jpg', 'Korketrekkeren', 'Oslo, Norway');
+    html += getImageDiv(50, 'jpg', 'Cheyenne', 'Colorado / Wyoming Border');
+    html += getImageDiv(47, 'jpg', 'Tour Eiffel', 'Paris, France');
+    html += getImageDiv(48, 'jpg', 'Château de Versailles', 'Versailles, France');
+    html += getImageDiv(1,  'jpg', 'Great Barrier Reef', 'Australia');
+    html += getImageDiv(2,  'png', 'Paradise', 'New Zealand');
+    html += getImageDiv(3,  'png', 'Oahu', 'New Zealand');
+    html += getImageDiv(4,  'png', 'Queenstown', 'New Zealand');
+    html += getImageDiv(5,  'png', 'St. Kilda Beach', 'Melbourne, Victoria, Australia');
+    html += getImageDiv(6,  'png', 'Melbourne Zoo', 'Melbourne, Victoria, Australia');
+    html += getImageDiv(7,  'png', 'Melbourne Zoo', 'Melbourne, Victoria, Australia');
+    html += getImageDiv(8,  'png', 'The Twelve Apostles', 'Victoria, Australia');
+    html += getImageDiv(9,  'png', 'Grampians National Park', 'Victoria, Australia');
+    html += getImageDiv(10, 'png', 'Grampians National Park', 'Victoria, Australia');
+    html += getImageDiv(11, 'png', 'Melbourne Cricket Ground', 'Melbourne, Victoria, Australia');
+    html += getImageDiv(13, 'png', 'Werribee Open Range Zoo', 'Werribee, Victoria, Australia');
+    html += getImageDiv(14, 'png', 'Werribee Open Range Zoo', 'Werribee, Victoria, Australia');
+    html += getImageDiv(15, 'png', 'Healesville Sanctuary', 'Healesville, Victoria, Australia');
+    html += getImageDiv(16, 'png', 'Healesville Sanctuary', 'Healesville, Victoria, Australia');
+    html += getImageDiv(17, 'png', 'Healesville Sanctuary', 'Healesville, Victoria, Australia');
+    html += getImageDiv(18, 'png', 'Blue Mountains', 'New South Wales, Australia');
+    html += getImageDiv(19, 'png', 'Sydney Opera House', 'Sydney, New South Wales, Australia');
+    html += getImageDiv(20, 'png', 'Sydney Opera House', 'Sydney, New South Wales, Australia');
+    html += getImageDiv(21, 'png', 'Harbour Bridge', 'Sydney, New South Wales, Australia');
+    html += getImageDiv(22, 'png', 'Flynn Reef', 'Great Barrier Reef, Australia');
+    html += getImageDiv(23, 'png', 'Flynn Reef', 'Great Barrier Reef, Australia');
+    html += getImageDiv(24, 'png', 'Cook Strait', 'New Zealand');
+    html += getImageDiv(25, 'png', 'Canterbury', 'New Zealand');
+    html += getImageDiv(26, 'png', 'Zirakzigil', 'New Zealand');
+    html += getImageDiv(27, 'png', 'Takaro Road', 'New Zealand');
+    html += getImageDiv(28, 'png', 'Milford Sound', 'New Zealand');
+    html += getImageDiv(29, 'png', 'Milford Sound', 'New Zealand');
+    html += getImageDiv(30, 'png', 'Milford Sound', 'New Zealand');
+    html += getImageDiv(31, 'png', 'New Zealand');
+    html += getImageDiv(32, 'png', 'New Zealand');
+    html += getImageDiv(33, 'png', 'New Zealand');
+    html += getImageDiv(34, 'png', 'New Zealand');
+    html += getImageDiv(35, 'png', 'New Zealand');
+    html += getImageDiv(36, 'png', 'New Zealand');
+    html += getImageDiv(37, 'jpg', 'Pawtuckaway State Park', 'New Hampshire, US');
+    html += getImageDiv(38, 'jpg', 'Pawtuckaway State Park', 'New Hampshire, US');
+    html += getImageDiv(39, 'jpg', 'Townsville', 'Queensland, Australia');
+    html += getImageDiv(40, 'jpg', 'Cozumel', 'Mexico');
+
+    $('#main').html(html);
+}
